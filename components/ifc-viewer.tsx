06a0e41dd6ca3dbb57b2028f927de4d7eabf4ebb
Louis Trümpler <78563314+louistrue@users.noreply.github.com>
2025-05-21 13:37:34 +0200

    fix: enable pointer events for the search bar in the IFC viewer

--- components/ifc-viewer.tsx
+++ components/ifc-viewer.tsx
@@ -1093,7 +1093,7 @@ function ViewerContent() {
                   onZoomSelected={handleZoomSelected}
                   isElementSelected={!!selectedElement}
                 />
-                <div className="absolute top-4 right-4">
+                <div className="absolute top-4 right-4 pointer-events-auto">
                   <ElementSearchBar />
                 </div>
               </>
