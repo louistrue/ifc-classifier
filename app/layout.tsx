944778394b0fe694357f63f63f90afaddb9473ba
Louis Trümpler <78563314+louistrue@users.noreply.github.com>
2025-06-11 15:42:11 +0200

    Revert "feat: integrate PostHog analytics by adding posthog-js and posthog-node dependencies, and update layout to include PostHogPageview component"

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,6 @@ import Script from "next/script";
 import Menubar from "@/components/layout/Menubar";
 import Footer from "@/components/layout/Footer";
 import { Analytics } from "@vercel/analytics/next";
-import { PHProvider, PostHogPageview } from "./providers";
-import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -39,24 +37,19 @@ export default function RootLayout({
         <meta name="theme-color" content="#ffffff" />
       </head>
       <body className={`${inter.className} h-full flex flex-col overflow-hidden`}>
-        <Suspense>
-          <PostHogPageview />
-        </Suspense>
-        <PHProvider>
-          <IFCContextProvider>
-            <I18nClientProvider>
-              <I18nProvider>
-                <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-                  <div className="flex flex-col h-full">
-                    <Menubar />
-                    <main className="flex-1 overflow-hidden">{children}</main>
-                    <Footer />
-                  </div>
-                </ThemeProvider>
-              </I18nProvider>
-            </I18nClientProvider>
-          </IFCContextProvider>
-        </PHProvider>
+        <IFCContextProvider>
+          <I18nClientProvider>
+            <I18nProvider>
+              <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+                <div className="flex flex-col h-full">
+                  <Menubar />
+                  <main className="flex-1 overflow-hidden">{children}</main>
+                  <Footer />
+                </div>
+              </ThemeProvider>
+            </I18nProvider>
+          </I18nClientProvider>
+        </IFCContextProvider>
         <Analytics />
         <Script src="https://cdn.jsdelivr.net/pyodide/v0.26.0/full/pyodide.js" strategy="beforeInteractive" />
       </body>
